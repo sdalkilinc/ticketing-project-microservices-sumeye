e1f915b4b3651d1c661ec430808458f7bf53caa4
Sumeye <dalkilincsumeye@gmail.com>
2023-01-24 21:17:00 -0500

    Initial commit

---
+++ src/main/java/com/cydeo/TicketingProjectMicroservicesSumeyeApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectMicroservicesSumeyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectMicroservicesSumeyeApplication.class, args);
+    }
+
+}
